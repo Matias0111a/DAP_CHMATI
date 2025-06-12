ec012ddd9bcbd494472d88186f37c6aa9f95bca5
Chaiman <48115713@est.ort.edu.ar>
2025-06-12 15:49:05 -0300

    aa

--- tp0login_home/loginhome1/lib/presentation/screens/add_screen.dart
+++ tp0login_home/loginhome1/lib/presentation/screens/add_screen.dart
@@ -2,14 +2,31 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:loginhome1/entities/bandas.dart';
 
-final bandasProvider = Provider<List<Banda>>((ref) => bandasList);
-
-class AddScreen extends ConsumerWidget {
-
+class AddScreen extends ConsumerStatefulWidget {
   static const String name = 'add_band_screen';
   const AddScreen({super.key});
+
   @override
-  Widget build(BuildContext context, WidgetRef ref){
+  ConsumerState<AddScreen> createState() => _AddScreenState();
+}
+
+class _AddScreenState extends ConsumerState<AddScreen> {
+  final TextEditingController nombreController = TextEditingController();
+  final TextEditingController integrantesController = TextEditingController();
+  final TextEditingController imageController = TextEditingController();
+  final TextEditingController originController = TextEditingController();
+
+  @override
+  void dispose() {
+    nombreController.dispose();
+    integrantesController.dispose();
+    imageController.dispose();
+    originController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text("Añadir Banda"),
@@ -19,9 +36,80 @@ class AddScreen extends ConsumerWidget {
           color: Colors.black,
           fontWeight: FontWeight.bold,
         ),
-      
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children:[
+            const Text('Ingrese los datos de la banda'),
+            const SizedBox(height: 20),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: TextField(
+                controller: nombreController,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  labelText: 'Nombre',
+                ),
+              ),
+            ),
+            const SizedBox(height: 20),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: TextField(
+                controller: integrantesController,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  labelText: 'Integrantes',
+                ),
+              ),
+            ),
+            const SizedBox(height: 20),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: TextField(
+                controller: imageController,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  labelText: 'Imagen URL',
+                ),
+              ),
+            ),
+            const SizedBox(height: 20),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: TextField(
+                controller: originController,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  labelText: 'Origen',
+                ),
+              ),
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                final nuevaBanda = Banda(
+                  nombre: nombreController.text,
+                  integrantes: integrantesController.text,
+                  image: imageController.text,
+                  origen: originController.text.isNotEmpty ? originController.text : null,
+                );
+                ref.read(bandasProvider.notifier).add(nuevaBanda);
+              },
+              child: const Text('Añadir Banda', style: TextStyle(fontSize: 20, color: Colors.black)),
+            )
+          ]
         )
+      ), 
     );
   }
-
 }
